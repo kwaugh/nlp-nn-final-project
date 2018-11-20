f474defbb37fe6cf35acb8b7338fbda598e08890
Keivaun Waugh <keivaunwaugh@gmail.com>
2018-11-20 17:08:03 -0600

    modularized code in parse_split.py

--- code/data/parse_split.py
+++ code/data/parse_split.py
@@ -2,27 +2,18 @@ import math
 
 num_splits = 12
 
-with open("clean-fr.txt", "r") as f:
-    lines = f.readlines()
-    split_size = math.ceil(len(lines) / float(num_splits))
+def split(input_file, output_prefix):
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+        split_size = math.ceil(len(lines) / float(num_splits))
 
-    for i in range(num_splits):
-        with open("fr_splits/" + str(i) + ".txt", "w") as g:
-            s = split_size * i
-            e = min(len(lines), (i + 1) * split_size)
+        for i in range(num_splits):
+            with open("{}/{}.txt".format(output_prefix, i), "w") as g:
+                s = split_size * i
+                e = min(len(lines), (i + 1) * split_size)
 
-            for j in range(s, e):
-                g.write(lines[j])
+                for j in range(s, e):
+                    g.write(lines[j])
 
-with open("clean-en.txt", "r") as f:
-    lines = f.readlines()
-    split_size = math.ceil(len(lines) / float(num_splits))
-
-    for i in range(num_splits):
-        with open("en_splits/" + str(i) + ".txt", "w") as g:
-            s = split_size * i
-            e = min(len(lines), (i + 1) * split_size)
-
-            for j in range(s, e):
-                g.write(lines[j])
-
+split("clean-fr.txt", "fr_splits")
+split("clean-en.txt", "en_splits")
