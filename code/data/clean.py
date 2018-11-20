b49a9d571767d2bb09e9e7348c818ad97288a0ba
John <fang97j@gmail.com>
2018-11-20 13:21:42 -0600

    update clean script to retain words, delete empty line pairs

--- code/data/clean.py
+++ code/data/clean.py
@@ -1,6 +1,5 @@
 import string
 import re
-from pickle import dump
 from unicodedata import normalize
 from collections import Counter
 
@@ -19,15 +18,15 @@ def load_doc(filename):
 def clean_lines(lines):
     cleaned = list()
     re_print = re.compile('[^%s]' % re.escape(string.printable))
-    table = str.maketrans('', '', string.punctuation)
+    # table = str.maketrans('', '', string.punctuation)
     for line in lines:
         line = normalize('NFD', line).encode('ascii', 'ignore')
         line = line.decode('UTF-8')
         line = line.split()
         line = [word.lower() for word in line]
-        line = [word.translate(table) for word in line]
+        # line = [word.translate(table) for word in line]
         line = [re_print.sub('', w) for w in line]
-        line = [word for word in line if word.isalpha()]
+        # line = [word for word in line if word.isalpha()]
         cleaned.append(' '.join(line))
     return cleaned
 
@@ -72,16 +71,38 @@ def update_dataset(lines, vocab):
 
 print("Starting reduction")
 
-en_filename = 'en.pkl'
+en_filename = 'clean-en.txt'
 en_vocab = to_vocab(en_lines)
 en_vocab = trim_vocab(en_vocab, 5)
 en_lines = update_dataset(en_lines, en_vocab)
-with open(en_filename, 'wb') as f:
-    dump(en_lines, f)
 
-fr_filename = 'fr.pkl'
+fr_filename = 'clean-fr.txt'
 fr_vocab = to_vocab(fr_lines)
 fr_vocab = trim_vocab(fr_vocab, 5)
 fr_lines = update_dataset(fr_lines, fr_vocab)
-with open(fr_filename, 'wb') as f:
-    dump(fr_lines, f)
+
+"""
+ctr = 0
+for idx, item in enumerate(fr_lines):
+    if not item and en_lines[idx]:
+        print("idx: ", idx)
+        print("en: ", en_lines[idx])
+        print("fr: ", fr_lines[idx])
+"""
+
+with open(en_filename, 'w') as en_f:
+    with open(fr_filename, 'w') as fr_f:
+        for idx in range(min(len(fr_lines), len(en_lines))):
+            en_line = en_lines[idx]
+            fr_line = fr_lines[idx]
+
+            if not en_line or not fr_line:
+                continue
+            
+            if en_line[-1] not in string.punctuation:
+                en_line = en_line + "."
+            if fr_line[-1] not in string.punctuation:
+                fr_line = fr_line + "."
+
+            en_f.write("%s\n" % en_line)
+            fr_f.write("%s\n" % fr_line)
