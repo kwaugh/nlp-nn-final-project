50443822526d2aea305f5cd8e26fd24504b42625
Keivaun Waugh <keivaunwaugh@gmail.com>
2018-11-22 12:13:42 -0600

    delete split.py and rename parse_split.py to split.py

--- code/data/split.py
+++ code/data/split.py
@@ -1,34 +1,19 @@
-from pickle import load, dump
-import random
+import math
 
-print("Starting splitting")
-en_lines = []
-with open('en.pkl', 'rb') as en_f:
-    en_lines = load(en_f)
+num_splits = 12
 
-fr_lines = []
-with open('fr.pkl', 'rb') as fr_f:
-    fr_lines = load(fr_f)
+def split(input_file, output_prefix):
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+        split_size = math.ceil(len(lines) / float(num_splits))
 
-idxs = [i for i in range(len(en_lines))]
-train_idxs = random.sample(idxs, int(0.8 * len(idxs)))
+        for i in range(num_splits):
+            with open("{}/{}.txt".format(output_prefix, i), "w") as g:
+                s = split_size * i
+                e = min(len(lines), (i + 1) * split_size)
 
-train_idxs_set = set(train_idxs)
-idxs = [x for x in idxs if x not in train_idxs_set]
+                for j in range(s, e):
+                    g.write(lines[j])
 
-valid_idxs = random.sample(idxs, int(0.5 * len(idxs)))
-
-valid_idxs_set = set(valid_idxs)
-test_idxs = [x for x in idxs if x not in valid_idxs_set]
-
-with open('train.pkl', 'wb') as f:
-    zipped = [(en_lines[i], fr_lines[i]) for i in train_idxs]
-    dump(zipped, f)
-
-with open('valid.pkl', 'wb') as f:
-    zipped = [(en_lines[i], fr_lines[i]) for i in valid_idxs]
-    dump(zipped, f)
-
-with open('test.pkl', 'wb') as f:
-    zipped = [(en_lines[i], fr_lines[i]) for i in test_idxs]
-    dump(zipped, f)
+split("clean-fr.txt", "fr_splits")
+split("clean-en.txt", "en_splits")
