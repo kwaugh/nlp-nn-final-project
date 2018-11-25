143226ba761d369ec457d416b080d390bbe9cc15
Keivaun Waugh <keivaunwaugh@gmail.com>
2018-11-25 15:33:42 -0600

    add option to specify what percentage of the data to use in the train/dev/test split.

--- code/data/train_dev_test_split.py
+++ code/data/train_dev_test_split.py
@@ -1,18 +1,18 @@
+import argparse
 import math
 
-num_splits = 12
-
 # file_prefix does not include file extension
-def train_dev_test_split(file_prefix, file_extension):
+def train_dev_test_split(file_prefix, file_extension, percentage):
     with open(file_prefix + file_extension, 'r') as f:
         lines = f.readlines()
-        train_lines = math.ceil(len(lines) * 0.8)
-        dev_lines = math.ceil(len(lines) * 0.1)
+        num_lines = int(len(lines) * percentage)
+        train_lines = math.ceil(num_lines * 0.8)
+        dev_lines = math.ceil(num_lines * 0.1)
 
         suffix_to_lines = {
                 'train': (0, train_lines),
                 'dev': (train_lines, train_lines + dev_lines),
-                'test': (train_lines + dev_lines, len(lines))
+                'test': (train_lines + dev_lines, num_lines)
         }
 
         for suffix, line_pos in suffix_to_lines.items():
@@ -21,7 +21,15 @@ def train_dev_test_split(file_prefix, file_extension):
                     g.write(lines[j])
 
 if __name__ == '__main__':
-    train_dev_test_split('english', '.txt')
-    train_dev_test_split('french', '.txt')
-    train_dev_test_split('english_parse', '.txt')
-    train_dev_test_split('french_parse', '.txt')
+    parser = argparse.ArgumentParser('description=train_dev_test_split.py')
+    parser.add_argument(
+        '--percentage',
+        dest='percentage',
+        type=float,
+        default=1.0,
+        help='What percentage of the europarl data should be used?')
+    args = parser.parse_args()
+    train_dev_test_split('english', '.txt', args.percentage)
+    train_dev_test_split('french', '.txt', args.percentage)
+    train_dev_test_split('english_parse', '.txt', args.percentage)
+    train_dev_test_split('french_parse', '.txt', args.percentage)
