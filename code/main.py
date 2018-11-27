1280a39d907b808d482a13f1e7ae2bbea6e97974
John Fang <fang97j@gmail.com>
2018-11-27 13:55:52 -0600

    Replace evaluate with bleu score

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ import time
 import torch
 import functools
 import pickle
+from nltk.translate.bleu_score import sentence_bleu
 from torch import optim
 from lf_evaluator import *
 from models import *
@@ -507,47 +508,36 @@ def train_model_encdec(
 # example with a valid denotation (if you've provided more than one).
 def evaluate(
         test_data, decoder, example_freq=50, print_output=True, outfile=None):
-    e = GeoqueryDomain()
     pred_derivations = decoder.decode(test_data)
-    selected_derivs, denotation_correct = e.compare_answers(
-            [ex.y for ex in test_data], pred_derivations)
-    num_exact_match = 0
-    num_tokens_correct = 0
-    num_denotation_match = 0
-    total_tokens = 0
+
+    bleus = []
     for i, ex in enumerate(test_data):
         if i % example_freq == 0:
             print('Example %d' % i)
             print('  x      = "%s"' % ex.x)
             print('  y_tok  = "%s"' % ex.y_tok)
-            print('  y_pred = "%s"' % selected_derivs[i].y_toks)
-        # Compute accuracy metrics
-        y_pred = ' '.join(selected_derivs[i].y_toks)
-        # Check exact match
-        if y_pred == ' '.join(ex.y_tok):
-            num_exact_match += 1
-        # Check position-by-position token correctness
-        num_tokens_correct += sum(a == b for a, b in zip(
-            selected_derivs[i].y_toks, ex.y_tok))
-        total_tokens += len(ex.y_tok)
-        # Check correctness of the denotation
-        if denotation_correct[i]:
-            num_denotation_match += 1
+            print('  y_pred = "%s"' % pred_derivations[i][0].y_toks)
+
+        hypotheses = [pred_derivations[i][0].y_toks]
+        reference = pred_derivations[i][0].example.y_tok
+        bleu = sentence_bleu(hypotheses, reference)
+        print("hypothesis: {}".format(hypotheses[0]))
+        print("reference: {}".format(reference))
+        print("bleu: {}".format(bleu))
+
+        bleus.append(bleu)
+
     if print_output:
-        print("Exact logical form matches: {}".format(
-            render_ratio(num_exact_match, len(test_data))))
-        print("Token-level accuracy: {}".format(
-            render_ratio(num_tokens_correct, total_tokens)))
-        print("Denotation matches: {}".format(
-            render_ratio(num_denotation_match, len(test_data))))
+        print("average bleu: {}".format(sum(bleus)/len(bleus)))
     # Writes to the output file if needed
+    """
     if outfile is not None:
         with open(outfile, "w") as out:
             for i, ex in enumerate(test_data):
                 out.write(ex.x + "\t" + " ".join(
                     selected_derivs[i].y_toks) + "\n")
         out.close()
-
+    """
 
 def render_ratio(numer, denom):
     return "%i / %i = %.3f" % (numer, denom, float(numer)/denom)
